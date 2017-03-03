4728d0e4a671ed34552c1e9361956af307d16573
Ksheerasagar Akella <Ksheerasagar.Akella@amdocs.com>
2017-03-02 20:55:15 -0800

    Fixed packages for the Application and its test

--- src/main/java/com/gagaovergigs/ms/provider/app/ProviderApplication.java
+++ src/main/java/com/gagaovergigs/ms/provider/app/ProviderApplication.java
@@ -1,4 +1,4 @@
-package com.gagaovergigs.ms;
+package com.gagaovergigs.ms.provider.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gagaovergigs/ms/provider/app/test/ProviderApplicationTests.java
+++ src/test/java/com/gagaovergigs/ms/provider/app/test/ProviderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gagaovergigs.ms;
+package com.gagaovergigs.ms.provider.app.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;

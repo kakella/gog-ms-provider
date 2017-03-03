17fc795008bfcab629dd2ff5387036a89b348c55
Ksheerasagar Akella <Ksheerasagar.Akella@amdocs.com>
2017-03-02 20:33:10 -0800

    Initial commit

---
+++ src/main/java/com/gagaovergigs/ms/ProviderApplication.java
@@ -0,0 +1,12 @@
+package com.gagaovergigs.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}

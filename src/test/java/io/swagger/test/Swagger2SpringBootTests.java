6188914caa9846e175f03b8c7e8f98eb5ace2768
Ksheerasagar Akella <Ksheerasagar.Akella@amdocs.com>
2017-03-03 01:06:08 -0800

    Changed Swagger codegen target language to Spring Boot

--- src/test/java/io/swagger/test/Swagger2SpringBootTests.java
+++ src/test/java/io/swagger/test/Swagger2SpringBootTests.java
@@ -1,4 +1,4 @@
-package com.gagaovergigs.ms.provider.app.test;
+package io.swagger.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProviderApplicationTests {
+public class Swagger2SpringBootTests {
 
     @Test
     public void contextLoads() {
